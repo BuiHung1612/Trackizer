f88e75d0dae580f838ba034514281c79597aac4a
HungBui1612 <hungbv@osp.com>
2024-06-12 15:21:02 +0700

    Initial Commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by HungBui on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
